942e18eac7f090d75ea4ad4dd9b410645e3730f1
apine <1985486089@qq.com>
2022-11-25 15:43:41 +0800

    :wrench: 配置eslint+prettier+husky+lint-staged

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    parserOptions: {
+        parser: '@typescript-eslint/parser',
+        ecmaFeatures: {
+            jsx: true
+        }
+    },
+    plugin: ['@typescript-eslint'],
+    rules: {
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+                js: 'never',
+                jsx: 'never',
+                ts: 'never',
+                tsx: 'never'
+            }
+        ]
+    },
+    settings: {
+        'import/resolver': {
+            typescript: {},
+            node: ['js','jsx','ts','tsx']
+        }
+    },
+    ignorePatterns: ['!.*', 'node_modules']
+}
---
+++ .prettierrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+    semi: false,
+    singleQuote: true,
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+    endOfLine: 'auto',
+    tabWidth: 4,
+    useTabs: false,
+    quoteProps: 'as-needed',
+    jsxSingleQuote: false,
+    trailingComma: 'none',
+    bracketSameLine: false,
+    rangeStart: 0,
+    rangeEnd: Infinity,
+    requirePragma: false,
+    insertPragma: false,
+    proseWrap: 'preserve',
+    htmlWhitespaceSensitivity: 'css'
+}
