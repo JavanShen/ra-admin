06cc59fc92a497fed036883fa44d596c8eb7cfeb
apine <1985486089@qq.com>
2022-11-26 23:04:53 +0800

    :wrench: 配置路径别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+
+const resolve = (dir: string) => path.join(__dirname, dir)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()]
+    plugins: [react()],
+    resolve: {
+        alias: [
+            {
+                find: '@',
+                replacement: resolve('src')
+            }
+        ]
+    }
 })
