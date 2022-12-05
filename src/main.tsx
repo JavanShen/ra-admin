79971cdb878af4f3292001d4e4d0cbb7d6b7f6e3
apine <1985486089@qq.com>
2022-12-05 16:02:15 +0800

    :sparkles: 主入口

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import '@/styles'
+
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import RouteTable from '@/router'
+import store from '@/store'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode></React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <RouteTable />
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>
 )
