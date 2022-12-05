366750b83baebb4e081ab350ea95be9973b42dd4
apine <1985486089@qq.com>
2022-12-05 14:22:07 +0800

    :sparkles: 新增路由表

---
+++ src/router/routes.ts
@@ -0,0 +1,34 @@
+import Loadable from '@/components/common/Loadable'
+
+const fixedRoutes = [
+    {
+        path: '/',
+        name: 'Layout',
+        component: Loadable({
+            loader: () => import('@/App')
+        })
+    },
+    {
+        path: 'login',
+        name: 'Login',
+        component: Loadable({
+            loader: () => import('@/view/login')
+        })
+    }
+]
+
+const asyncRoutes = [
+    {
+        path: 'user',
+        name: 'User',
+        component: Loadable({
+            loader: () => import('@/view/user')
+        }),
+        meta: {
+            title: '用户',
+            roles: ['admin']
+        }
+    }
+]
+
+export { fixedRoutes, asyncRoutes }
