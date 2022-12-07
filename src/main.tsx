1ad5ad981ec850156dad588b5e36fb34218dd77f
apine <1985486089@qq.com>
2022-12-07 11:03:16 +0800

    :truck: 路径调整

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import '@/styles'
+import '@/styles/main.css'
 
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
