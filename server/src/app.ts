12c5b19e835b74dcc30702144eae5aa90f2f454b
apine <1985486089@qq.com>
2022-12-12 21:55:08 +0800

    :building_construction: 结构更改

---
+++ server/src/app.ts
@@ -0,0 +1,5 @@
+#!/user/bin/env node
+import server from './server.js'
+;(function start() {
+    server()
+})()
---
+++ server/src/server.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+import history from 'connect-history-api-fallback'
+import path from 'path'
+
+const __dirname = path.resolve()
+
+const server = () => {
+    const app = express()
+
+    app.use(history())
+
+    app.use(express.static('dist'))
+
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'dist/index.html'))
+    })
+
+    app.listen(3000, () => {
+        console.log('server start')
+    })
+}
+
+export default server
