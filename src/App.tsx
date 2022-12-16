543540eeeccde65ef5b4a57b85d2249a55fd0b97
apine <1985486089@qq.com>
2022-12-16 20:15:00 +0800

    :art: 使用theme高阶组件

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,32 @@ import { BrowserRouter } from 'react-router-dom'
 import RouteTable from '@/router'
 import { Provider } from 'react-redux'
 import store from '@/store'
+import useStoreSelector from '@/hooks/useStoreSelector'
 
-import Theme from './Theme'
+import theme from '@/hocs/theme'
+
+const Router = () => (
+    <BrowserRouter>
+        <RouteTable />
+    </BrowserRouter>
+)
+
+const CustomTheme = () => {
+    const { color } = useStoreSelector('config')
+
+    const Theme = theme(Router, {
+        token: {
+            colorPrimary: color
+        }
+    })
+
+    return <Theme />
+}
 
 const App = () => {
     return (
         <Provider store={store}>
-            <Theme>
-                <BrowserRouter>
-                    <RouteTable />
-                </BrowserRouter>
-            </Theme>
+            <CustomTheme />
         </Provider>
     )
 }
