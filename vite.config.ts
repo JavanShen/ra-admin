0085d0bc3ee2d4a692a92a9bca4a9c046dd2bf19
apine <1985486089@qq.com>
2022-12-18 17:09:57 +0800

    :wrench: 配置跨域代理

--- vite.config.ts
+++ vite.config.ts
@@ -14,5 +14,13 @@ export default defineConfig({
                 replacement: resolve('src')
             }
         ]
+    },
+    server: {
+        proxy: {
+            '/api/': {
+                target: 'http://localhost:3000',
+                changeOrigin: true
+            }
+        }
     }
 })
