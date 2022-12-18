18e75aca64aa3086082cbabcb3937ea10245b285
apine <1985486089@qq.com>
2022-12-18 18:10:37 +0800

    :adhesive_bandage: 添加中间件忽略报错

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
 import routerReducer from './router'
 import configReducer from './config'
+import userReducer from './user'
 
 export default configureStore({
     reducer: {
         router: routerReducer,
-        config: configReducer
-    }
+        config: configReducer,
+        user: userReducer
+    },
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware({
+            serializableCheck: false
+        })
 })
