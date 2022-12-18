e05cc427984d1f50ed175d0eb4f9a0ccdc866af5
apine <1985486089@qq.com>
2022-12-18 18:13:29 +0800

    :sparkles: 路由守卫添加权限验证，路由生成

--- src/hooks/useRouterGuard.ts
+++ src/hooks/useRouterGuard.ts
@@ -1,11 +1,50 @@
 import { useEffect } from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { whiteList, notFoundRoute } from '@/router/routes'
+import { getToken } from '@/utils/auth'
+import useStoreSelector from '@/hooks/useStoreSelector'
+import { getUserInfo } from '@/api/user'
+import { useDispatch } from 'react-redux'
+import { setUserInfo } from '@/store/user'
+import { addRoutes, addRoute } from '@/store/router'
+import { generateRoutes } from '@/router/utils'
 
-const useRouterGuard = () => {
+import type { Location } from 'react-router-dom'
+
+const useRouterGuard = (to: Location) => {
     const { pathname } = useLocation()
+    const { userInfo } = useStoreSelector('user')
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
 
     useEffect(() => {
-        console.log('location change')
+        const permission = async () => {
+            if (whiteList.includes(pathname)) return
+
+            const token = getToken()
+
+            if (token === null) return navigate('/login', { replace: true })
+
+            if (userInfo.role) {
+                return
+            } else {
+                const { code, data } = await getUserInfo()
+
+                if (code === 1) {
+                    const routes = generateRoutes(data.role)
+
+                    dispatch(addRoutes(routes))
+                    dispatch(addRoute(notFoundRoute))
+
+                    dispatch(setUserInfo(data))
+                    navigate(to.pathname)
+                } else {
+                    navigate('/login')
+                }
+            }
+        }
+
+        permission()
     }, [pathname])
 }
 
