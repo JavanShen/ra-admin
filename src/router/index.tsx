81f9b9ec1e95b7f74223688e6288e96cc1923c55
apine <1985486089@qq.com>
2022-12-18 18:14:57 +0800

    :sparkles: 处理子路由，记录历史

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,23 +1,31 @@
 import { Routes, Route } from 'react-router-dom'
 import useRouterGuard from '@/hooks/useRouterGuard'
 import useStoreSelector from '@/hooks/useStoreSelector'
+import useRecordHistory from '@/hooks/useRecordHistory'
+import { useLocation } from 'react-router-dom'
+
+import type { Route as R } from '@/types/router'
+
+const renderRoutes = (routes: R[]) => {
+    return routes.map(route => (
+        <Route
+            key={route.name}
+            path={route.path}
+            element={<route.component />}
+            {...(route.children ? renderRoutes(route.children) : null)}
+        ></Route>
+    ))
+}
 
 const RouteTable = () => {
     const { routes } = useStoreSelector('router')
+    const location = useLocation()
 
-    useRouterGuard()
+    useRecordHistory()
 
-    return (
-        <Routes>
-            {routes.map(route => (
-                <Route
-                    key={route.name}
-                    path={route.path}
-                    element={<route.component />}
-                />
-            ))}
-        </Routes>
-    )
+    useRouterGuard(location)
+
+    return <Routes>{renderRoutes(routes)}</Routes>
 }
 
 export default RouteTable
