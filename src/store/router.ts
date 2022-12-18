8702b80a7d6d57c786be569e1b6cad2e81cc38fd
apine <1985486089@qq.com>
2022-12-18 17:21:51 +0800

    :sparkles: 路由store新增路由历史数据， 添加多个路由方法，历史覆盖，历史增加方法

--- src/store/router.ts
+++ src/store/router.ts
@@ -1,18 +1,38 @@
 import { createSlice } from '@reduxjs/toolkit'
-import { fixedRoutes } from '@/router/routes'
+import { fixedRoutes, asyncRoutes } from '@/router/routes'
+
+import type { PayloadAction } from '@reduxjs/toolkit'
+import type { Route } from '@/types/router'
+import type { Location } from 'react-router-dom'
 
 export const routerSlice = createSlice({
     name: 'router',
     initialState: {
-        routes: fixedRoutes
+        routes: fixedRoutes,
+        location: {
+            last: <Partial<Location>>{},
+            current: <Partial<Location>>{}
+        }
     },
     reducers: {
-        addRoute: (state, action) => {
+        addRoute: (state, action: PayloadAction<Route>) => {
             state.routes.push(action.payload)
+        },
+        addRoutes: (state, action: PayloadAction<Route[]>) => {
+            state.routes = [...state.routes, ...action.payload]
+        },
+        pushLocation: (state, action: PayloadAction<Location>) => {
+            state.location.last = state.location.current
+            state.location.current = action.payload
+        },
+        replaceLocation: (state, action: PayloadAction<Location>) => {
+            state.location.last = {}
+            state.location.current = action.payload
         }
     }
 })
 
-export const { addRoute } = routerSlice.actions
+export const { addRoute, addRoutes, pushLocation, replaceLocation } =
+    routerSlice.actions
 
 export default routerSlice.reducer
