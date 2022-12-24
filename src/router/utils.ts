ba17cb6be70698ef1f285b10dfbfce86ab54dad9
apine <1985486089@qq.com>
2022-12-24 16:21:40 +0800

    :adhesive_bandage: 使用immer实现不可变数据

--- src/router/utils.ts
+++ src/router/utils.ts
@@ -1,17 +1,21 @@
 import { asyncRoutes } from './routes'
+import produce from 'immer'
 
 import type { Route } from '@/types/router'
 import type { Role } from '@/types/user'
 
 const filterRoutesByRole = (routes: Route[], role: Role) => {
-    return routes.reduce((res, route) => {
-        if (route.meta?.roles.includes(role)) {
-            const { children } = route
-            if (children) route.children = filterRoutesByRole(children, role)
-            res.push(route)
-        }
-        return res
-    }, <Route[]>[])
+    return produce(routes, draft =>
+        draft.reduce((res, route) => {
+            if (route.meta?.roles.includes(role)) {
+                const { children } = route
+                if (children)
+                    route.children = filterRoutesByRole(children, role)
+                res.push(route)
+            }
+            return res
+        }, <Route[]>[])
+    )
 }
 
 const generateRoutes = (role: Role) => {
