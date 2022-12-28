69933ab199d9e000b53339c1948ef6d6ed843555
apine <1985486089@qq.com>
2022-12-28 15:48:10 +0800

    :sparkles: 更新 Loading 组件，添加加载动画

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { memo } from 'react'
+import { Spin } from 'antd'
+import { LoadingOutlined } from '@ant-design/icons'
 
-const Loading: React.FC = () => {
-    return <div>加载中</div>
+const LoadingIcon = <LoadingOutlined style={{ fontSize: 62 }} />
+
+const Loading = () => {
+    return (
+        <Spin
+            style={{
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)'
+            }}
+            indicator={LoadingIcon}
+        />
+    )
 }
 
-export default Loading
+export default memo(Loading)
