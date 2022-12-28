01e43e9b12bf8c3301445503da3ca8ac015a31c3
apine <1985486089@qq.com>
2022-12-28 15:51:30 +0800

    :dizzy::heavy_plus_sign: 添加路由过渡动画

--- src/layout/components/AppMain.tsx
+++ src/layout/components/AppMain.tsx
@@ -1,10 +1,30 @@
-import { Outlet } from 'react-router-dom'
+import { useRef } from 'react'
+import { useOutlet, useLocation } from 'react-router-dom'
+import { SwitchTransition, CSSTransition } from 'react-transition-group'
+
+const duration = 300
 
 const AppMain = () => {
+    const { pathname } = useLocation()
+    const currentOutlet = useOutlet()
+    const nodeRef = useRef(null)
+
     return (
-        <section>
-            <Outlet />
-        </section>
+        <SwitchTransition>
+            <CSSTransition
+                key={pathname}
+                nodeRef={nodeRef}
+                timeout={duration}
+                classNames="page"
+                unmountOnExit
+            >
+                {() => (
+                    <section className="page" ref={nodeRef}>
+                        {currentOutlet}
+                    </section>
+                )}
+            </CSSTransition>
+        </SwitchTransition>
     )
 }
 
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,6 +1,8 @@
 import { Layout, theme } from 'antd'
 import { AppMain, SideBar } from './components'
 
+import './styles.css'
+
 const { Header, Content, Sider } = Layout
 
 const LayOut = () => {
@@ -15,7 +17,13 @@ const LayOut = () => {
                     <SideBar />
                 </Sider>
                 <Layout>
-                    <Content>
+                    <Content
+                        style={{
+                            padding: 24,
+                            margin: 0,
+                            backgroundColor: '#f5f5f5'
+                        }}
+                    >
                         <AppMain />
                     </Content>
                 </Layout>
